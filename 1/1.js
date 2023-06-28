d3fb4696f0818c591ed16ee1d4e675f9c937aa9f
Valentina <valenag.0703@gmail.com>
2023-06-28 15:21:24 -0500

    taller objetos

---
+++ 1/1.js
@@ -0,0 +1,7 @@
+// 1. Cree un objeto donde se almacenen los productos de un supermercado con su respectivo 
+// precio. Losproductos son:
+// Papa, Arroz, Lentejas, Aceite. Luego imprima cada uno de los precios almacenados en el 
+// objeto anterior con su respectivo producto.
+
+let objeto ={papa:1000 , arroz: 1200, lentejas: 3000, aceite:5000};
+console.log(objeto);
---
+++ 2/2.js
@@ -0,0 +1,6 @@
+// 2. Cree un objeto vacío, p.e. let objeto = {} y agregue tres pares clave-valor. Imprima 
+// todo el objeto para verificar que se han agregado las nuevos pares clave-valor
+
+let objeto = {}
+ objeto= {tres: 3, dos: 2, uno:1}
+ console.log(objeto);
